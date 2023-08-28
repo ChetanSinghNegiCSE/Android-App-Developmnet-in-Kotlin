c07604d99fb9ec4e25d4ea1fabe1a8cb53a5c99b
ChetanSinghNegiCSE <chetansinghnegicse@gamil.com>
2023-08-28 16:42:36 +0530

    Activity LifeCycle

--- AppDevNotes/ActivityLifecycle/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ AppDevNotes/ActivityLifecycle/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.Button
@@ -12,11 +13,14 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     private  var  msg=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("MY TAG","MainActivity : onCreate")
 
         val tv_hello=findViewById<TextView>(R.id.tvHello)
         val et_name = findViewById<EditText>(R.id.etName)
@@ -50,6 +54,45 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("MSG",msg)
             startActivity(intent)
         }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MY TAG","MainActivity : onStart ")
+    }
+
+
+
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MY TAG","MainActivity : onResume ")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MY TAG","MainActivity : OnPause ")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MY TAG","MainActivity : onStop ")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MY TAG","MainActivity : onDestroy ")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MY TAG","MainActivity : onRestart ")
 
     }
 }
