96530edf921418f6dc0ced92dfc3a263ac4130c8
ChetanSinghNegiCSE <chetansinghnegicse@gamil.com>
2023-08-29 18:17:06 +0530

    Functions VS Methods

--- AppDevNotes/OOPDemo/app/src/main/java/com/example/oopdemo/Car.kt
+++ AppDevNotes/OOPDemo/app/src/main/java/com/example/oopdemo/Car.kt
@@ -4,6 +4,20 @@ import android.util.Log
 
 
 //Function
+
+/*function is a peace of code that is called by name
+ it can be passed data to operate on
+ and can optionally return data.
+*/
+
+/*Method  is a peace of code that is called by name
+  associated(related) with an object
+  method is also a function
+
+  IN General Methods are functions that belong to a class
+
+  but not all functions are methods*/
+
 open class Car {
     var maxSpeed = 150
     open fun start(){
@@ -11,4 +25,6 @@ open class Car {
         Log.i("MyTag","Car Max Speed is $maxSpeed")
 
     }
+
+
 }
