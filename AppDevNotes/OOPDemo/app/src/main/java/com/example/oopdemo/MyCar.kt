5106e42bf367231e8dc4ff8ba625a9523fa828ce
ChetanSinghNegiCSE <chetansinghnegicse@gamil.com>
2023-08-29 17:59:05 +0530

    Interface

--- AppDevNotes/OOPDemo/app/src/main/java/com/example/oopdemo/MyCar.kt
+++ AppDevNotes/OOPDemo/app/src/main/java/com/example/oopdemo/MyCar.kt
@@ -3,11 +3,19 @@ package com.example.oopdemo
 import android.util.Log
 
 
-//Inheritance
-class MyCar : Car() {
+//Inheritance,Inheritance with interface
+class MyCar : Car(),SpeedController {
 
     //Overriding Functions
     override fun start() {
-        Log.i("MyTag","MyCar is Starting.....")
+        Log.i("MyTag","MyCar is Starting..... and Brand ID is ${getBrandID()} ")
+    }
+
+    override fun absFun1() {
+
+    }
+
+    override fun absFun2() {
+
     }
 }
---
+++ AppDevNotes/OOPDemo/app/src/main/java/com/example/oopdemo/SpeedController.kt
@@ -0,0 +1,13 @@
+package com.example.oopdemo
+
+//Interfaces
+
+interface SpeedController {
+
+    fun absFun1()
+    fun absFun2()
+
+    fun getBrandID():String{
+        return "JJKB2321"
+    }
+}
