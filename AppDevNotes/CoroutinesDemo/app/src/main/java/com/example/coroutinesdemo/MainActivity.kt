961d20514c9b821cc679be638762249dda20e1d8
ChetanSinghNegiCSE <chetansinghnegicse@gmail.com>
2023-09-08 18:55:36 +0530

    change the thread of a coroutine

--- AppDevNotes/CoroutinesDemo/app/src/main/java/com/example/coroutinesdemo/MainActivity.kt
+++ AppDevNotes/CoroutinesDemo/app/src/main/java/com/example/coroutinesdemo/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /* We Should always implement long running tasks asynchronously in a separate thread
    to achieve that we use coroutines
@@ -15,6 +16,7 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
+    private lateinit var tvDownload : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.tvCount)
         val countButton = findViewById<Button>(R.id.btnCount)
         val downloadButton = findViewById<Button>(R.id.btnDownload)
+        tvDownload=findViewById(R.id.tvDownload)
 
         countButton.setOnClickListener {
             textView.text = count++.toString()
@@ -33,10 +36,19 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-}
+//we can't directly  call a view component from background thread
+    private suspend fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
 
-private fun downloadUserData() {
-    for (i in 1..200000) {
-        Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){//Switching Threads
+
+                tvDownload.text="Downloading user $i"
+
+        }
+    }
+
+
     }
 }
+
