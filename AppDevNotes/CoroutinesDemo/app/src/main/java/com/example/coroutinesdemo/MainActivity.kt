cd07b8a5e533af0248e86b588bdfd64fc6e54e85
ChetanSinghNegiCSE <chetansinghnegicse@gmail.com>
2023-09-08 18:00:34 +0530

    Starter Code

--- AppDevNotes/CoroutinesDemo/app/src/main/java/com/example/coroutinesdemo/MainActivity.kt
+++ AppDevNotes/CoroutinesDemo/app/src/main/java/com/example/coroutinesdemo/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.coroutinesdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+/* We Should always implement long running tasks asynchronously in a separate thread
+   to achieve that we use coroutines
+ */
 
 class MainActivity : AppCompatActivity() {
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.tvCount)
+        val countButton = findViewById<Button>(R.id.btnCount)
+        val downloadButton = findViewById<Button>(R.id.btnDownload)
+
+        countButton.setOnClickListener {
+            textView.text = count++.toString()
+        }
+        downloadButton.setOnClickListener {
+            downloadUserData()
+        }
+    }
+}
+
+private fun downloadUserData() {
+    for (i in 1..200000) {
+        Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
     }
 }
